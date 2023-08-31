a33cfb8f45be133f9e478670def3fa4b897d18d6
Mohamad Abdou <mohamed22_1@hotmail.com>
2023-08-31 19:52:26 +0300

    Add username to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root 'main#index'
   get 'main/abou_us'
 end
